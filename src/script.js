b2e644f61efa83777bd62847956cbdf8ec2618e5
Vlove21 <153148892+Vlove21@users.noreply.github.com>
2026-04-11 10:08:50 -0400

    Updated html and script

--- src/script.js
+++ src/script.js
@@ -2,7 +2,6 @@ function displayInfo() {
   ///Sydney
 
   let sydneyElement = document.querySelector("#sydney");
-
   let sydneyTime = sydneyElement.querySelector(".time");
   let sydneyDateElement = sydneyElement.querySelector(".date");
   let sydneyDate = moment().tz("Australia/Sydney");
@@ -24,22 +23,56 @@ displayInfo();
 setInterval(displayInfo, 1000);
 
 function updateCity(event) {
-  let cityTz = event.target.value;
+  let cityTz = null;
+  cityTz = event.target.value;
+  console.log(cityTz);
   if (cityTz === "current") {
     cityTz = moment.tz.guess();
   }
   if (cityTz === "city") {
     cityTz = moment.tz.guess();
   }
-  let cityElement = document.querySelector("#cities");
-  let cityTime = moment().tz(cityTz).format("h:mm:ss [<small>]A[</small>]");
-  let cityDate = moment().tz(cityTz).format("MMMM Do, YYYY");
-  let cityName = cityTz.replace("_", " ").split("/")[1];
-  cityElement.innerHTML = `<div class="time-clock" >
+
+  function updateTime() {
+    let cityElement = document.querySelector("#cities");
+    let cityTime = moment().tz(cityTz).format("h:mm:ss [<small>]A[</small>]");
+    let cityDate = moment().tz(cityTz).format("MMMM Do, YYYY");
+    let cityName = cityTz.replace("_", " ").split("/")[1];
+    cityElement.innerHTML = `<div class="time-clock" >
         <div class="city-info">
     <div class="city"> ${cityName}<div class="date">${cityDate}</div></div>
     <div class="time" id="time"> ${cityTime}
-   </div></div></div>`;
+   </div></div></div> 
+    <a href="/">All cities</a>`;
+
+    if (
+      cityElement ===
+      `<div class="time-clock" >
+        <div class="city-info">
+    <div class="city"> ${cityName}<div class="date">${cityDate}</div></div>
+    <div class="time" id="time"> ${cityTime}
+   </div></div></div> 
+    <a href="/">All cities</a>`
+    ) {
+      setTimeout();
+    }
+    console.log(cityTz);
+  }
+  //let selectChange = document.querySelector("#location")
+  //selectChange.addEventListener("change", )
+  //clearInterval();
+
+  //updateTime();
+  //timeSelect.addEventListener("change", clearInterval(updateTime));
+  let intervalidId = null;
+
+  function startInterval() {
+    if (intervalidId !== null) {
+      clearInterval(intervalidId);
+    }
+    intervalidId = setInterval(updateTime, 1000);
+  }
+  startInterval();
 }
 
 let timeSelectElement = document.querySelector("#location");
